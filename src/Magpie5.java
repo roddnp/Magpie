ab94c66150a9531d0f8e2420683c0c6d6aae9c8f
Nathan <nathan@Ds-MacBook-Air.local>
2018-11-12 20:15:54 -0700

    adding 5

--- src/Magpie5.java
+++ src/Magpie5.java
@@ -250,6 +250,9 @@ public class Magpie5 {
 	}
 
 	private String[] randomResponses = { "Interesting, tell me more", "Hmmm.",
-			"Do you really think so?", "You don't say." };
+			"Do you really think so?", "You don't say.","Thats neat",
+			"Wow",
+			"really?",
+			"cool"" };
 
 }
